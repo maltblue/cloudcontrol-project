90e3c70b6e12aca06ddce347a92d864d05353f35
Matthew Setter <matthew.setter@gmail.com>
2011-12-07 15:53:57 +0000

    First addition of the bootstrap file to the project

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title>Malt Blue / Cloud Control :: Test Project</title>
+</head>
+<body>
+	<h1>Malt Blue / Cloud Control :: Test Project</h1>
+	<p>Hello World</p>
+</body>
+</html>
