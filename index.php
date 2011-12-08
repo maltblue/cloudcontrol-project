845e13e9e1956f8b9989f9e3bfe6511317912482
Matthew Setter <matthew.setter@gmail.com>
2011-12-08 10:20:51 +0000

    added staging notice

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
-	<title>Malt Blue / Cloud Control :: Test Project</title>
+	<title>Malt Blue / Cloud Control :: Test Project (Staging)</title>
 </head>
 <body>
-	<h1>Malt Blue / Cloud Control :: Test Project</h1>
+	<h1>Malt Blue / Cloud Control :: Test Project (Staging)</h1>
 	<p>Hello World</p>
 </body>
 </html>
