5ba39d158060cd09d676e933e11249dde79912b1
Matthew Setter <matthew.setter@gmail.com>
2011-12-09 09:27:29 +0000

    The core setup for the application. Contains:  - configuration  - functions  - key includes  - data load scripts  - add-on css

---
+++ config.php
@@ -0,0 +1,78 @@
+<?php
+define('APPLICATION_PATH', '.');
+define('TABLE_STAFF', 'staff');
+
+$zfPath = APPLICATION_PATH . '/library';
+require_once($zfPath . '/Zend/Loader/Autoloader.php');
+$autoloader = Zend_Loader_Autoloader::getInstance();
+
+// database configuration
+$options = array(Zend_Db::AUTO_QUOTE_IDENTIFIERS => false);
+$pdoParams = array(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true);
+$config = new Zend_Config(
+    array(
+        'database' => array(
+            'adapter' => 'Pdo_Mysql',
+            'params'  => array(
+                'host'     => '127.0.0.1',
+                'username' => 'dep5jfkw8gf',
+                'password' => 'grSyvSLuShGyHhp',
+                'dbname'   => 'dep5jfkw8gf',
+                'options'  => $options,
+                'driver_options' => $pdoParams,
+            )
+        )
+    )
+);
+ 
+$db = Zend_Db::factory($config->database);
+$db->getConnection();
+$db->setFetchMode(Zend_Db::FETCH_OBJ);
+
+$view = new Zend_View();
+
+function getManageForm()
+{
+    $firstName = new Zend_Form_Element_Text('firstName', 
+        array('label' => 'first name: ', 'required' => true)
+    );
+    $lastName = new Zend_Form_Element_Text('lastName', array('label' => 'last name: '));
+    $emailAddress = new Zend_Form_Element_Text('emailAddress', 
+        array('label' => 'email address: ', 'required' => true)
+    );
+    $occupation = new Zend_Form_Element_Text('occupation', array('label' => 'occupation: '));    
+    $id = new Zend_Form_Element_Hidden('id', array('required' => true));
+    $submit = new Zend_Form_Element_Submit('submit', 
+        array('label' => 'Create User', 'ignore' => true)
+    );
+    
+    $form = new Zend_Form;
+    $form->setMethod('post')
+         ->setAttrib('id', 'manage_user')
+         ->addElement($firstName)
+         ->addElement($lastName)
+         ->addElement($emailAddress)
+         ->addElement($occupation)
+         ->addElement($submit)
+         ->addElement($id);
+         
+    return $form;
+}
+
+function getDeleteForm()
+{
+    $id = new Zend_Form_Element_Text('id', 
+        array('required' => true, 'readonly' => true, 'label' => 'user id: ')
+    );
+    $submit = new Zend_Form_Element_Submit('submit', 
+        array('label' => 'Delete User', 'ignore' => true)
+    );
+    
+    $form = new Zend_Form;
+    $form->setMethod('post')
+         ->setAttrib('id', 'delete_user')
+         ->addElement($id)
+         ->addElement($submit);
+         
+    return $form;
+}
---
+++ data/db/dbload.sql
@@ -0,0 +1,25 @@
+-- database table
+
+DROP TABLE staff;
+
+-- the staff table
+CREATE TABLE staff (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  firstName VARCHAR(100) NOT NULL,
+  lastName VARCHAR(100),
+  emailAddress VARCHAR(150) NOT NULL,
+  occupation VARCHAR(50),
+  CONSTRAINT UNIQUE INDEX uniq_email(emailAddress)
+) ENGINE = InnoDB, COMMENT = "Store list of staff members";
+
+INSERT INTO staff (firstName, lastName, emailAddress, occupation) 
+VALUES ("Muhammad","Ali","mali@gmail.com","Boxer"),
+("Lou","Ambers","lambers@gmail.com","Boxer"),
+("Vito","Antuofermo","vantuofermo@gmail.com","Boxer"),
+("Jorge","Arce","j.arce@gmail.com","Boxer"),
+("Alexis","Arguello","a.arguello@gmail.com","Boxer"),
+("Henry","Armstrong","h.armstrong@gmail.com","Boxer"),
+("Abe","Attell","a.attell@gmail.com","Boxer"),
+("Monte","Attell","m.attell@gmail.com","Boxer"),
+("Yuri","Arbachakov","y.arbachakov@gmail.com","Boxer"),
+("Satoshi","Aragaki","s.aragaki@gmail.com","Boxer");
